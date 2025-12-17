f63252889ef445a17fb8f1d0d3c33b5f47c795a8
MatthewYao2000 <matthew.yao@gmail.com>
2025-12-17 15:14:44 -0500

    dec 17

--- auth-service/src/main/java/com/unicourse/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/unicourse/auth_service/AuthServiceApplication.java
@@ -138,7 +138,7 @@ public class AuthServiceApplication {
 	download google chat and google doc 
 
 	resume -> java (8, 17, 21) spring boot 2.7 + spring cloud + data base (relational , non relational (not must)) + S3 (must) + monitoring(aws cloud watch)
-		+testing(junit, mokito )   + aws services  + personal exp (python or AI,DL, RL, ...)
+		+testing(junit, mokito )   + aws services  + personal exp (python or AI,DL, RL, ...) + frontend(react,  angular.. must)
 	
 	-> link -> submit your resume -> hard to modify 
 	-> marketing name -> matthew yao ... and marketing email, marketing phone number -> location, city + sate.... edu.... 
@@ -189,7 +189,11 @@ High performance ->
 	
 	
 	producer -> kafka cluster[broker0 -> [topicA-partition0 (leader), ], broker1->[topic A - parition1], broker2 ->[topic partition2] ] ->consumer group[consumer1, 2,..]
-	
+
+	what is consumer group
+	one or more than one consumer(s) -> services or instances that work together to consume message from a topic
+		each message in a topic is processed by only ONE consumer within the group -> offset -> commit 
+		if there are multiple consumer groups, each group gets its own copy of me messages -> offset
 	offset -> message index -> __consumer_offsets -> integer -> the position of a message in the partition log 
 
 	how consumers consume message
@@ -205,7 +209,195 @@ High performance ->
 
 
 		paritioning strategy: 
+			1: if a partition is specified in the record, use this partition
+			2: if no partition is specified but a key is present  choose a partition based hashing of key and mod operation
+			3: if not partition and key is present choose the partition based on round robin strategy
+
+
+		message deliver strategy:
+			acks = 0 producer does not wait for any ack, 
+			acks =1 produer waits for ack from the leader only
+			acks = all or -1 : producer waits until the leader and all followers acknowledge the message 
+
+			1: at most once:消息最多递交和处理一次
+			
+			2: at least once ： 消息至少发送一次
+
+			3:excatly once ： 消息只能被递交一次
+				idempotence : PID -》
+			
 			
 
+	
+*/
+
+
+/*
+docker and k8s
+isolation and restriction
+
+namespace - isolation : container A cannot see container B's process, files and network....
+
+cgroups - restriction
+	used to control how much CPU, memory, disk IO, networking bandwith.... a container can use
+
+
+	three main components
+	Image -> special type of filesystem
+		an operating system -> kernel and user space
+			kernel -> hardware
+			user space - where application runs
+		kernel -> root filesysmtem -> user space
+		an image  includes: programs, lib, resources, config..., env variabels..... volumes
+		an image contians no dynamic data,  and it is read only and does not change once its built
+
+	container :
+	runtime instance of an image
+	in java: class and object. class = image, and object = container 
+	you can create and stop and delete and pause, just like process 
+
+	repository
+	a centralized place to store docker images
+	docker hub -> docker registry 
+	<repository> : <tag>
+	mysql : 5.1.2 -> mysql version 5.1.2
+
+
+
+docker file
+text file -> instructions  -> how to build a docker image
+docker pull, docker run docker push 
+
+volumes -> data storage area managed by docker
+shared and reusable: a volume can be mounted to multiple contianers and allowing data to be sahred across them.
+
+bind mounts vs volumes
+bind mounts is managed by op, and volumes is managed by docker
+
+docker compose
+yaml -> version : "3.0"
+services: 
+	web:
+		ports:
+			-"8080":"3000"
+
+	db:
+		image: portgres:16
+		environment:
+				user: postgres
+				password:123
+		volumes:
+			- "your path"
+
+docker compose 
+
+
+K8s:
+	Node: fundamental compute unit 
+	type: control plane node(master node) and worker node
+
+	worker node: components
+		kubelet: primary agent on every worker node. it can communicates with api server in master node and send commands to continer runtime.
+		kube proxy: ip tables - used to forward traffic to where you want to communicate
+		container runtime - responsible for runnning containers..
+
+		kubelet - manager, overseeing pod lifecycle and status and communication with api server and send commands
+		container runtime - it is the executor
+		kube proxy  - networking coordinator managing service routing and load balancing
+
+
+		master node
+		api server - it is the single entry point for communication for all cluster components and external user( you )
+		it handles rest requests, vlaidates api object .... for the cluster
+
+		etcd - key value store and used to save ALL state and configuration data for ENTIRE k8s cluster..
+
+		scheduler - respnosible for assigning newly created pods and node in the cluter. 
+		
+		controller  manager - it controls all controllers - > node controler, repliucaset controller endpoint....
+
+
+		
+
+		kubectl exec -it ngix:1.2
+		general steps ->
+		1 kubectl -> api server. before api server sends this command, we need to do authentication and authorization.
+		2: api server forwards to node (api server -> kubelet)
+		3: kubelet communicates with container runtime(kubelet -> runtime)
+		4: runtime launches process (runtime -> container)
+		5: data forward back (container -> kubelet -> api server -> your terminal )
+
+		communication in k8s:
+		1: control plane communication
+				master node's components -> https/restful api
+		2: data plance communication 
+			between pods- > tcp
+
+		
+
+YAML
+	apiVersion
+		api group and version that a k8s resources use
+		pod -> V1
+		service -> V1
+		deployment apps/V1
+
+	kind
+		pod
+		service
+		deployment
+
+	metadata
+		name UID, namespace -> all about description about the resouces
+		labels are used identifu k8s resources
+		does not change any behaves about your resources
+			labels:
+				app: antra-service
+				env: prod
+				version 2.1
+		kubeclt get pods -l app = antra-service
+		..... env = prod
+
+		spec
+		defines the desired state ofo the resource
+			how many replicas you want
+			which container image a pod should run
+			... bahaves
+			resource limit -> cpu memory.
+		
+		namespace : isolation
+			kubectl get pods -> no resources found in defulat namespace.
+			kubectl get namespace
+				defualt, kube-node, kube public, kube-system
+
+			
+	
+		liveness and readiness in k8s
+
+			liveness: checks if the container is alive, if the not, k8s consider the contianer to be in unusable state
+
+			readiness: checks if the container is ready
+
+
+			spec:
+				livenessProbe:
+					httpGet:
+						path: /health/live
+						port: 8080
+					initialDelaySeconds: 15 启动contianer之后 15 只后开始启动liveness 检查
+					periodSecond：20  每20秒检查一次
+					failureThreshold： 3， 连续三次失败后重启contianer。。
+
+				readinessProbe
+
+
+	volume:
+		如果pod重新启动，但是你的volume 不会被删除，pod 的bahaves 还是一样的
+		但是如果你的pod 被彻底删除，相对应的volume 才会被销毁
+
+		一个pod可以有多个volume
+
+	
+		
 
 */
